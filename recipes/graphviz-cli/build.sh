9309b0320c526a7cd29980bd53a30970768bd8a8
Stuart Berg <bergs@janelia.hhmi.org>
2016-06-03 11:46:38 -0400

    graphviz: On OS X, get the 'expat' dependency from conda, not the system.

--- recipes/graphviz-cli/build.sh
+++ recipes/graphviz-cli/build.sh
@@ -27,6 +27,13 @@ make
 # This is failing for rtest.
 # Doesn't do anything for the rest
 # make check
+
+if [[ $(uname) == Darwin ]]; then
+    # For some reason, libexpat.1.dylib can't be found without setting the RPATH,
+    # and it even prevents 'make install' from working.
+    install_name_tool -add_rpath ${PREFIX}/lib cmd/dot/.libs/dot
+fi
+
 make install
 
 dot -c
